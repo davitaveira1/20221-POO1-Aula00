7a1551b79d2d32866aafe03e19a9df73fbd29580
Davi Taveira <davitaveira@gmail.com>
2022-03-04 21:28:38 -0300

    Projeto criado

---
+++ src/Aplicacao/Ex01.java
@@ -0,0 +1,14 @@
+package Aplicacao;
+
+public class Ex01 {
+    
+    public static void main(String[] args) {
+        
+        String nome;
+        nome = "Davi";
+        System.out.println(nome);
+        
+        
+    }
+    
+}
