a99ffbbeb9cf9ccd04394aa001f83530d2fe50fd
Davi Taveira <davitaveira@gmail.com>
2022-03-11 16:41:28 -0300

    Ajuste

---
+++ src/Aplicacao/Ex02.java
@@ -0,0 +1,18 @@
+package Aplicacao;
+
+public class Ex02 {
+    
+    public static void main(String[] args) {
+        
+       double n1,n2,media;
+       
+       n1 = 10;
+       n2 = 8;
+       
+       media = (n1+n2)/2;
+       
+        System.out.println("Media: "+media);
+        
+    }
+    
+}
